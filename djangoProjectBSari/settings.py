f96d214bc0a4c593c52a1c5972165d3958633d1d
bsarifhsu <b_sari@mail.fhsu.edu>
2023-11-01 23:19:05 -0500

    Debug is False. End of Django Tutorial Part 4.

--- djangoProjectBSari/settings.py
+++ djangoProjectBSari/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%1t0m+tdsla^3&8u81tnvqhx+95!1fi=(2krcszsm-tnpdj)b6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
